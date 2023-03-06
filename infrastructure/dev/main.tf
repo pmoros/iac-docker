7a3d25b6c6f52a0165f7f8e8486876fe519d3387
Paul Moros <pmoros@outlook.com>
2023-03-06 09:45:52 -0500

    feat: add resource group

---
+++ infrastructure/dev/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = "=1.3.9"
+  backend "azurerm" {
+  }
+}
+
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-${var.project}-${var.environment}-${var.location}"
+  location = var.location
+  tags     = var.tags
+}
---
+++ infrastructure/dev/terraform.tfvars
@@ -0,0 +1,10 @@
+project = "activity4"
+location = "eastus"
+environment = "dev"
+# Tags
+tags = {
+  environment = "dev"
+  costcenter  = "it"
+  lob         = "acad"
+  location    = "eastus"
+}
