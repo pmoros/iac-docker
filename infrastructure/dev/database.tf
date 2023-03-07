e80c7639db37b357b3dc31afedafc110472586fc
Paul Moros <pmoros@outlook.com>
2023-03-07 08:22:00 -0500

    feat: create todos database

--- infrastructure/dev/database.tf
+++ infrastructure/dev/database.tf
@@ -34,3 +34,30 @@ resource "azurerm_mysql_firewall_rule" "azure-services" {
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "0.0.0.0"
 }
+
+# Store database credentials in key vault
+resource "azurerm_key_vault_secret" "db_username" {
+  name         = "dbusername"
+  value        = var.administrator_login
+  key_vault_id = azurerm_key_vault.kv.id
+
+  depends_on = [azurerm_key_vault_access_policy.defined_access_policy]
+}
+
+
+resource "azurerm_key_vault_secret" "db_password" {
+  name         = "dbpassword"
+  value        = random_password.database_password.result
+  key_vault_id = azurerm_key_vault.kv.id
+
+  depends_on = [azurerm_key_vault_access_policy.defined_access_policy]
+}
+
+# Create database
+resource "azurerm_mysql_database" "todos" {
+  name                = "todos"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_mysql_flexible_server.mysql.name
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
+}
