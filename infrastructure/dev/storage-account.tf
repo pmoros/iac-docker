64dd2db3ac5029f897a4173b60ea3606d49cdbd5
Paul Moros <pmoros@outlook.com>
2023-03-06 22:49:24 -0500

    fix: remove storage account

--- infrastructure/dev/storage-account.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_storage_account" "st" {
-  name                     = "st${var.owner}${var.project}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = var.location
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
-  min_tls_version          = "TLS1_2"
-  tags                     = var.tags
-}
